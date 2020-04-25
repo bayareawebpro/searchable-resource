edc5de795c2df60171ef8d65065cecf6bd99c7ca
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-24 18:25:19 -0700

    Add Coverage
    - Add provides test
    - Declare Laravel 8 Support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -32,22 +32,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
     }
 
     /**
-     * Define environment setup.
-     * @param  Application  $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        // Setup default database to use sqlite :memory:
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
-    }
-
-    /**
      * Setup the test environment.
      */
     protected function setUp(): void
