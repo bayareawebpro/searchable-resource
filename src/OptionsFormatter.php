5fbd6ef05cc2cba8794fccd25377803b4d8f5d6d
Dan Alvidrez <dan@bayareawebpro.com>
2021-03-16 04:05:57 -0700

    Update OptionsFormatter.php

--- src/OptionsFormatter.php
+++ src/OptionsFormatter.php
@@ -11,13 +11,57 @@ class OptionsFormatter implements FormatsOptions {
     public function __invoke(string $key, Collection $options): Collection
     {
         if($key === 'per_page'){
-            return $options->map(fn($value, $key)=>[
-                'label' => Str::title("$value / Page"),
-                'value' => $value,
-            ]);
+            return $this->append($options, "/ Page");
         }
+        return $this->titleCase($options);
+    }
+
+    /**
+     * @param Collection $options
+     * @param string $append
+     * @return Collection
+     */
+    protected function append(Collection $options, string $append): Collection
+    {
         return $options->map(fn($value, $key)=>[
+            'label' => "$value $append",
+            'value' => $value,
+        ]);
+    }
+
+    /**
+     * @param Collection $options
+     * @param string $label
+     * @return Collection
+     */
+    protected function nullable(Collection $options, string $label = 'All'): Collection
+    {
+        return $options->prepend([
+            'label' => $label,
+            'value' => null,
+        ]);
+    }
+
+    /**
+     * @param Collection $options
+     * @return Collection
+     */
+    protected function titleCase(Collection $options): Collection
+    {
+        return $options->map(fn($value, $key) => [
             'label' => Str::title(str_replace("_", " ", "$value")),
+            'value' => $value,
+        ]);
+    }
+
+    /**
+     * @param Collection $options
+     * @return Collection
+     */
+    protected function literal(Collection $options): Collection
+    {
+        return $options->map(fn($value, $key) => [
+            'label' => $value,
             'value' => $value,
         ]);
     }
